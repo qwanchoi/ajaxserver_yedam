1ce7bf827c9065c6bc8045e1e1386e27a94a22b8
cyannara <cyannara@naver.com>
2021-12-11 06:59:35 +0900

    first

---
+++ myserver/src/main/java/com/yedam/myserver/users/web/UserController.java
@@ -0,0 +1,42 @@
+package com.yedam.myserver.users.web;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yedam.myserver.users.mapper.UserMapper;
+import com.yedam.myserver.users.vo.UserVO;
+
+@CrossOrigin(origins = {"http://127.0.0.1:3000"}, maxAge = 3600)
+@RestController
+public class UserController {
+
+	@Autowired UserMapper mapper;
+	
+	@GetMapping("/userSelect")
+	public List<Map> userSelectList(UserVO vo) {
+		return mapper.findByName(vo);
+	}
+		
+	@PostMapping("/userInsert")
+	public UserVO userInsert(UserVO vo) {
+		 mapper.persist(vo);
+		 return vo;
+	}
+	
+	@PostMapping("/userUpdate")
+	public UserVO userUpdate(UserVO vo) {
+		 mapper.merge(vo);
+		 return vo;
+	}	
+	@GetMapping("/userDelete")
+	public UserVO userDelete(UserVO vo) {
+		 mapper.remove(vo);
+		 return vo;
+	}		
+}
